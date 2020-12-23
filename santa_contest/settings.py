cd51370a850bcd79ea58e4c06868e37140d3aa7a
anazaret <achille.nazaert@gmail.com>
2020-12-23 23:27:40 +0100

    Add home page

--- santa_contest/settings.py
+++ santa_contest/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'santa_contest.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['santa_contest/templates/'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
